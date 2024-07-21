d63f614c13b10dc6b7f8015f4a1ea9ba677afed0
Srinidhi Sivakaminathan <srinidhisivakaminathan@gmail.com>
2024-07-21 13:39:44 -0500

    removed cors middleware

--- MyApp.py
+++ MyApp.py
@@ -1,29 +1,13 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
 import datetime
 
 app = FastAPI()
 app.mount("/static_assets", StaticFiles(directory="static_assets"), name="static_assets")
 
-origins = [ # Define a list of origins (URLs) that are allowed to make cross-origin requests to the application
-    "http://localhost",
-    "http://localhost:8080",
-	"http://localhost.tiangolo.com",
-	"https://localhost.tiangolo.com",
-	"http://localhost:63342",
-	"http://127.0.0.1:8080"
-]
 
-app.add_middleware( # Add CORS middleware to the FastAPI application (`app`) with specific configurations
-    CORSMiddleware, # Use the CORSMiddleware provided by FastAPI for handling CORS
-    allow_origins=origins,  # Allow requests from the origins specified in the `origins` list
-    allow_credentials=True, # Allow credentials (like cookies, HTTP authentication) in cross-origin requests
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.) in cross-origin requests
-    allow_headers=["*"], # Allow all HTTP headers in cross-origin requests
-)
 
 
 
--- static_assets/scriptCOPY.js
+++ static_assets/scriptCOPY.js
@@ -1,5 +1,6 @@
 function searchRecipe() {
-    var searchTerm = document.getElementById('searchInput').value.trim(); // Get the value from the input field with id 'searchInput' and remove any leading/trailing whitespace
+    var searchTerm = document.getElementById('searchInput').value.trim();
+     // Get the value from the input field with id 'searchInput' and remove any leading/trailing whitespace
     if (searchTerm !== '') {
         fetchRecipe(searchTerm); // Call the fetchRecipe function, passing the searchTerm as an argument
     } else {
@@ -8,10 +9,14 @@ function searchRecipe() {
 }
 function displayRecipe(recipeData) {
     console.log('Fetch response data:', recipeData); // Log the fetched recipe data to the console for debugging purposes
-    document.getElementById('name').innerText = recipeData[0];  // Set the inner text of the element with id 'name' to the 1st item in the recipeData array
-    document.getElementById('description').innerText = recipeData[1]; // Set the inner text of the element with id 'description' to the 2nd item in the recipeData array
-    document.getElementById('steps').innerText = recipeData[2]; // Set the inner text of the element with id 'steps' to the 3rd item in the recipeData array
-    document.getElementById('ingredients').innerText = recipeData[3]['Ingredients'];  // Set the inner text of the element with id 'ingredients' to the 'Ingredients' property of the 4th item in the recipeData array
+    document.getElementById('name').innerText = recipeData[0];
+     // Set the inner text of the element with id 'name' to the 1st item in the recipeData array
+    document.getElementById('description').innerText = recipeData[1];
+    // Set the inner text of the element with id 'description' to the 2nd item in the recipeData array
+    document.getElementById('steps').innerText = recipeData[2];
+    // Set the inner text of the element with id 'steps' to the 3rd item in the recipeData array
+    document.getElementById('ingredients').innerText = recipeData[3]['Ingredients'];
+    // Set the inner text of the element with id 'ingredients' to the 'Ingredients' property of the 4th item in the recipeData array
 }
 
 function displayRecipeNotFound(searchTerm) {
@@ -25,15 +30,19 @@ function displayRecipeNotFound(searchTerm) {
         // Set the inner text of the element with id 'steps' to an empty string, clearing any previous content
 }
 
-async function fetchRecipe(searchTerm) { // async function can execute tasks that takes time (like fetching data from a server) without blocking the main execution thread of the program.
+async function fetchRecipe(searchTerm) {
+// async function can execute tasks that takes time (like fetching data from a server) without blocking the main
+// execution thread of the program.
     var url = 'http://127.0.0.1:8000/recipe/' + encodeURIComponent(searchTerm);
-      // Construct the URL for the API endpoint. encodeURIComponent() ensures that special characters in the searchTerm are properly encoded for inclusion in a URL.
+      // Construct the URL for the API endpoint. encodeURIComponent() ensures that special characters in the searchTerm
+      // are properly encoded for inclusion in a URL.
 fetch(url)    // Perform a fetch request to the constructed URL
     .then(response => response.json())
             // When the fetch operation completes successfully, convert the response to JSON format
     .then(
            data => displayRecipe(data.out[0] )
-           ) // Chain another .then() to handle the parsed JSON data. Pass the first item of the 'out' array (from the fetched data) to the displayRecipe function.
+           ) // Chain another .then() to handle the parsed JSON data. Pass the first item of the 'out' array
+           // (from the fetched data) to the displayRecipe function.
     .catch(error => displayRecipeNotFound(searchTerm)); // If an error occurs during the fetch operation or JSON parsing, show recipe not found.
 }
 
